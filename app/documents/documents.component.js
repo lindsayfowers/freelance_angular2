8447067e8179ff858e91a7fcb6e4a243e0469d49
lindsayfowers <lfowers@gmail.com>
2017-03-07 13:01:16 -0700

    Setting up API connection between angular2 app and rails app

--- app/documents/documents.component.js
+++ app/documents/documents.component.js
@@ -5,35 +5,29 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
+var Rx_1 = require("rxjs/Rx");
+var document_service_1 = require("./document.service");
 var DocumentsComponent = (function () {
-    function DocumentsComponent() {
+    function DocumentsComponent(documentService) {
+        this.documentService = documentService;
         this.pageTitle = "Document Dashboard";
-        this.documents = [
-            {
-                title: "My First Doc",
-                description: "asdfasdf",
-                file_url: 'http://google.com',
-                updated_at: '03/02/17',
-                image_url: 'https://upload.wikimedia.org/wikipedia/commons/d/d5/Mistakes-to-avoid-when-hiring-freelancers.jpg',
-            },
-            {
-                title: "My Second Doc",
-                description: "asdfasdf",
-                file_url: 'http://google.com',
-                updated_at: '03/02/17',
-                image_url: 'https://blogst.workana.com/blog/wp-content/uploads/2012/10/freelancer-sketch.png',
-            },
-            {
-                title: "My Last Doc",
-                description: "asdfasdf",
-                file_url: 'http://google.com',
-                updated_at: '03/02/17',
-                image_url: 'https://www.ticketbooth.com.au/ticketing/wp-content/uploads/2016/02/photodune-7575550-human-resources-and-selfdevelopment-modern-business-resized-1.jpg',
-            }
-        ];
+        this.mode = "Observable";
     }
+    DocumentsComponent.prototype.ngOnInit = function () {
+        var _this = this;
+        var timer = Rx_1.Observable.timer(0, 5000);
+        timer.subscribe(function () { return _this.getDocuments(); });
+    };
+    DocumentsComponent.prototype.getDocuments = function () {
+        var _this = this;
+        this.documentService.getDocuments()
+            .subscribe(function (documents) { return _this.documents = documents; }, function (error) { return _this.errorMessage = error; });
+    };
     return DocumentsComponent;
 }());
 DocumentsComponent = __decorate([
@@ -41,8 +35,10 @@ DocumentsComponent = __decorate([
         moduleId: module.id,
         selector: 'documents',
         templateUrl: 'documents.component.html',
-        styleUrls: ['documents.component.css']
-    })
+        styleUrls: ['documents.component.css'],
+        providers: [document_service_1.DocumentService]
+    }),
+    __metadata("design:paramtypes", [document_service_1.DocumentService])
 ], DocumentsComponent);
 exports.DocumentsComponent = DocumentsComponent;
 //# sourceMappingURL=documents.component.js.map
--- app/documents/documents.component.ts
+++ app/documents/documents.component.ts
@@ -1,36 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Rx';
 import { Document } from './document';
+import { DocumentService } from './document.service';
 
 @Component({
   moduleId: module.id,
   selector: 'documents',
   templateUrl: 'documents.component.html',
-  styleUrls: ['documents.component.css']
+  styleUrls: ['documents.component.css'],
+  providers: [ DocumentService ]
 })
-export class DocumentsComponent {
+export class DocumentsComponent implements OnInit {
   pageTitle: string = "Document Dashboard"
-  
-  documents: Document[] = [
-    {
-      title: "My First Doc",
-      description: "asdfasdf",
-      file_url: 'http://google.com',
-      updated_at: '03/02/17',
-      image_url: 'https://upload.wikimedia.org/wikipedia/commons/d/d5/Mistakes-to-avoid-when-hiring-freelancers.jpg',
-    },
-    {
-      title: "My Second Doc",
-      description: "asdfasdf",
-      file_url: 'http://google.com',
-      updated_at: '03/02/17',
-      image_url: 'https://blogst.workana.com/blog/wp-content/uploads/2012/10/freelancer-sketch.png',
-    },
-    {
-      title: "My Last Doc",
-      description: "asdfasdf",
-      file_url: 'http://google.com',
-      updated_at: '03/02/17',
-      image_url: 'https://www.ticketbooth.com.au/ticketing/wp-content/uploads/2016/02/photodune-7575550-human-resources-and-selfdevelopment-modern-business-resized-1.jpg',
-    }
-  ]
+  documents: Document[];
+  errorMessage: string;
+  mode = "Observable";
+
+  constructor(
+    private documentService: DocumentService;
+  ) {}
+
+  ngOnInit() {
+    let timer = Observable.timer(0, 5000);
+    timer.subscribe(() => this.getDocuments());
+  }
+
+  getDocuments() {
+    this.documentService.getDocuments()
+        .subscribe(
+          documents => this.documents = documents,
+          error => this.errorMessage = <any>error
+        );
+  }
 }
